d8f4ec62de63d2e4a37ab2284ca10408e0fda1ae
Dmitriy <volga19171@gmail.com>
2021-06-20 19:47:01 +0400

    блок 5: выполнено задание 11, реализованы флюент-проверки

---
+++ src/main/java/dto/Groups.java
@@ -0,0 +1,35 @@
+package dto;
+
+import com.google.common.collect.*;
+
+import java.util.*;
+
+public class Groups extends ForwardingSet<GroupData> {
+
+    private Set<GroupData> delegate;
+
+    @Override
+    protected Set<GroupData> delegate() {
+        return delegate;
+    }
+
+    public Groups(Groups groups) {
+        this.delegate = new HashSet<GroupData>(groups.delegate);
+    }
+
+    public Groups() {
+        this.delegate = new HashSet<GroupData>();
+    }
+
+    public Groups withAdded(GroupData group) {
+        Groups groups = new Groups(this);
+        groups.add(group);
+        return groups;
+    }
+
+    public Groups without(GroupData group) {
+        Groups groups = new Groups(this);
+        groups.remove(group);
+        return groups;
+    }
+}
