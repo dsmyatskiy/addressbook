33b43b5d4ee8a54e58d5128afc65feaf5e2d334f
Dmitriy <volga19171@gmail.com>
2021-06-27 17:43:53 +0400

    l6_m4: в параметризованном тесте реализовано чтение данных из файла

--- src/test/java/tests/GroupCreationTest.java
+++ src/test/java/tests/GroupCreationTest.java
@@ -4,6 +4,7 @@ import appmanager.*;
 import dto.*;
 import org.testng.annotations.*;
 
+import java.io.*;
 import java.util.*;
 
 import static org.hamcrest.CoreMatchers.*;
@@ -12,11 +13,15 @@ import static org.hamcrest.MatcherAssert.*;
 public class GroupCreationTest extends TestBase {
 
     @DataProvider
-    public Iterator<Object[]> validGroups() {
+    public Iterator<Object[]> validGroups() throws IOException {
         List<Object[]> list = new ArrayList<>();
-        list.add(new Object[]{new GroupData().withName("test1").withHeader("header1").withFooter("footer1")});
-        list.add(new Object[]{new GroupData().withName("test2").withHeader("header2").withFooter("footer3")});
-        list.add(new Object[]{new GroupData().withName("test3").withHeader("header3").withFooter("footer3")});
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.csv")));
+        String line = reader.readLine();
+        while (line != null) {
+            String[] split = line.split(";");
+            list.add(new Object[]{new GroupData().withName(split[0]).withHeader(split[1]).withFooter(split[2])});
+            line = reader.readLine();
+        }
         return list.iterator();
     }
 
