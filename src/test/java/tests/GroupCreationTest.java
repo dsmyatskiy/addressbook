841f3581b474a7276d0ef5f38cbcb2ebc000c93f
Dmitriy <volga19171@gmail.com>
2021-06-27 17:33:14 +0400

    l6_m4: реализован параметризованный тест для создания групп

--- src/test/java/tests/GroupCreationTest.java
+++ src/test/java/tests/GroupCreationTest.java
@@ -4,17 +4,26 @@ import appmanager.*;
 import dto.*;
 import org.testng.annotations.*;
 
+import java.util.*;
+
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.*;
 
 public class GroupCreationTest extends TestBase {
 
-    @Test
-    public void groupCreationTest() {
-        app.goTo().groupPage();
+    @DataProvider
+    public Iterator<Object[]> validGroups() {
+        List<Object[]> list = new ArrayList<>();
+        list.add(new Object[]{new GroupData().withName("test1").withHeader("header1").withFooter("footer1")});
+        list.add(new Object[]{new GroupData().withName("test2").withHeader("header2").withFooter("footer3")});
+        list.add(new Object[]{new GroupData().withName("test3").withHeader("header3").withFooter("footer3")});
+        return list.iterator();
+    }
 
+    @Test(dataProvider = "validGroups")
+    public void groupCreationTest(GroupData group) {
+        app.goTo().groupPage();
         Groups before = app.group().all();
-        GroupData group = new GroupData().withName("my");
         app.group().create(group);
         app.goTo().groupPage();
         assertThat(app.group().count(), equalTo(before.size() + 1));
